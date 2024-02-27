736d322491b45984f8497ddb81b3b198bb8031fd
rawr51919 <colton51919@gmail.com>
2024-02-27 19:37:10 -0400

    remove unnecessary js

--- nonmin/scripts.js
+++ nonmin/scripts.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll('.ContactButtons button');
-    buttons.forEach(button => {
-        button.addEventListener('mouseenter', function () {
-            const videoLink = button.getAttribute('data-video-link');
-            button.setAttribute('title', videoLink);
-        });
-    });
-});
 function sillyGoose() {
     alert("You should have seen this coming -_-");
 }
