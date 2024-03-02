56774f7f9b8c87b452e5cfb622f7fdb00e7dad9d
rawr51919 <colton51919@gmail.com>
2024-03-02 18:46:05 -0400

    static generation of nonce

--- nonmin/scripts.js
+++ nonmin/scripts.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Generate nonce value
-    let array = new Uint8Array(16);
-    window.crypto.getRandomValues(array);
-    let nonce = btoa(String.fromCharCode.apply(null, array));
-    // Set nonce attribute for script tag
-    document.querySelector('script').setAttribute('nonce', nonce);
-    // Update CSP with the generated nonce
-    let cspMetaTag = document.querySelector('meta[http-equiv="Content-Security-Policy"]');
-    let cspContent = cspMetaTag.getAttribute('content').replace('nonce-placeholder', 'nonce-' + nonce);
-    cspMetaTag.setAttribute('content', cspContent);
-});
 function sillyGoose() {
     alert("You should have seen this coming -_-");
 }
