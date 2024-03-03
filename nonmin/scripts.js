6dc4c713c756984a674b3ec5a8bd27dfbfc6f817
rawr51919 <colton51919@gmail.com>
2024-03-03 14:17:58 -0400

    remove unsafe-inline in csp default-src and generate nonces in-browser when js is enabled

--- nonmin/scripts.js
+++ nonmin/scripts.js
@@ -1,3 +1,21 @@
+// Function to generate a random nonce value
+function generateNonce() {
+    let array = new Uint32Array(8);
+    window.crypto.getRandomValues(array);
+    let nonce = '';
+    for (let value of array) {
+        nonce += ('0000' + value.toString(16)).slice(-4);
+    }
+    return nonce;
+}
+// Get the script elements
+let scriptElements = document.querySelectorAll('script');
+// Generate nonce
+let nonce = generateNonce();
+// Set nonce attribute for each script element
+scriptElements.forEach(function (script) {
+    script.setAttribute('nonce', "nonce-" + nonce);
+});
 function sillyGoose() {
     alert("You should have seen this coming -_-");
 }
