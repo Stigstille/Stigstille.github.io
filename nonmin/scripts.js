93c1ecc88deb45ed2f935de96170d664872f6f03
rawr51919 <colton51919@gmail.com>
2024-03-05 18:11:07 -0400

    comment out console.log in discordStat, rework CSP to use hashes instead of nonces, and adjust CSP headers to match

--- nonmin/scripts.js
+++ nonmin/scripts.js
@@ -1,30 +1,3 @@
-// Function to generate a random nonce value
-function generateNonce() {
-    let array = new Uint32Array(8);
-    window.crypto.getRandomValues(array);
-    let nonce = '';
-    for (let value of array) {
-        nonce += ('0000' + value.toString(16)).slice(-4);
-    }
-    return nonce;
-}
-// Get the script elements
-let scriptElements = document.querySelectorAll('script');
-// Generate nonce
-let nonce = generateNonce();
-// Set nonce attribute for each script element
-scriptElements.forEach(function (script) {
-    script.setAttribute('nonce', "nonce-" + nonce);
-});
-// Call the function so nonce attributes are set for dark reader styles too
-updateStyleNonce();
-// function to do the above
-function updateStyleNonce() {
-    let styles = document.querySelectorAll('style.darkreader.darkreader--sync');
-    styles.forEach(function (style) {
-        style.setAttribute('nonce', "nonce-" + nonce);
-    });
-}
 document.addEventListener('DOMContentLoaded', function () {
     let favoriteVideoButton = document.getElementById('favorite-video-button');
     favoriteVideoButton.addEventListener('click', function (event) {
